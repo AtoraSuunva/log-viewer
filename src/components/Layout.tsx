f317acb35d536d76aadbca9742bd23ec6b74d212
AtoraSuunva <at@atora.dev>
2025-02-27 06:19:35 -0500

    feat(layout): use pretty • character

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -27,11 +27,11 @@ export default function Layout() {
         </ErrorBoundary>
       </main>
       <footer>
-        By <a href="https://atora.dev">atora.dev</a> -{' '}
+        By <a href="https://atora.dev">atora.dev</a> •{' '}
         <a href="https://github.com/widgetbot-io/message-renderer">
           Message Renderer
         </a>{' '}
-        - Source: <a href="https://github.com/AtoraSuunva/log-viewer">GitHub</a>
+        • Source: <a href="https://github.com/AtoraSuunva/log-viewer">GitHub</a>
       </footer>
     </>
   )
